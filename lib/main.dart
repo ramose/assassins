ea6230118c7d0fc9058b9b8d1080fc18e5e22a8b
Leo <leopapilaya@gmail.com>
2023-01-19 09:52:18 +0800

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,30 +17,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class CounterCubit extends Cubit<int> {
+  CounterCubit({this.initialData = 20}) : super(initialData);
+
+  int initialData;
+  int? current = 0;
+  int? next = 0;
+
+  void tambahData() {
+    emit(state + 1);
+  }
+
+  void kurangData() {
+    emit(state - 1);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    print("Rebuild");
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Stream App!"),
-        ),
-        body: StreamBuilder(builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return const Center(
-              child: Text(
-                "{snapshot.data.toString()}",
-                style: TextStyle(fontSize: 50),
-              ),
-            );
-          }
-        }));
+  void onChange(Change<int> change) {
+    // TODO: implement onChange
+    super.onChange(change);
+    print(change);
+    current = change.currentState;
+    next = change.nextState;
   }
 }
 
@@ -50,12 +50,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  Stream<int> countStream() async* {
-    for (int i = 1; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      yield i;
-    }
-  }
+  CounterCubit mycounter = CounterCubit(initialData: 0);
+
+  // Stream<int> countStream() async* {
+  //   for (int i = 1; i < 10; i++) {
+  //     await Future.delayed(const Duration(seconds: 1));
+  //     yield i;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -64,22 +66,47 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: const Text("Stream App!"),
       ),
-      body: StreamBuilder(
-        stream: countStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return Center(
-              child: Text(
-                "${snapshot.data}",
-                style: const TextStyle(fontSize: 50),
-              ),
-            );
-          }
-        },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Padding(padding: const EdgeInsets.all(20), child: Text('${mycounter.current}', style: const TextStyle(fontSize: 40),),),
+          BlocBuilder(
+            bloc: mycounter,
+            buildWhen: ((previous, current) {
+              
+            }),
+            builder: (context, state) {
+              return Column(
+                
+                children: [
+                  Center(
+                    child: mycounter.current! < 5
+                        ? Text(
+                            "$state",
+                            style: const TextStyle(fontSize: 50),
+                          )
+                        : Text("YOU WIN!"),
+                  ),
+                ],
+              );
+            },
+          ),
+          Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            mycounter.kurangData();
+                          },
+                          icon: const Icon(Icons.remove)),
+                      IconButton(
+                          onPressed: () {
+                            mycounter.tambahData();
+                          },
+                          icon: const Icon(Icons.add))
+                    ],
+                  )
+        ],
       ),
     );
   }
